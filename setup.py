39dd3edd01eb7df93f1f11869e9c5f1ec1a1cc69
Gioele La Manno <gioelelamanno@gmail.com>
2017-08-09 10:47:34 +0200

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.1.0"
+
+setup(
+	name="regressionmix",
+	version=__version__,
+	packages=find_packages(),
+	# metadata
+	author="Linnarsson Lab",
+	author_email="sten.linnarsson@ki.se",
+	description="Fork of linear-regression mix",
+	license="MIT",
+	url="https://github.com/linnarsson-lab/linear-regression-mix",
+)
